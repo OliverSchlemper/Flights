64469c607af95eca930e4a69128590a0c021b8ae
OliverSchlemper <oliver.schlemper@fau.de>
2024-05-16 14:39:16 +0200

    Revert "this is the version i want to restore after my testing"
    This reverts commit b1c6e5b0fecc29ceea11623bb6bfcb03d1bea9bd.

--- Flight.py
+++ Flight.py
@@ -244,23 +244,18 @@ class Flight:
         con.close()
 
     #------------------------------------------------------------------------------------------------------
-    def plot_event_by_id(self=None, i=None, station_number=None, run_number=None, event_number=None, lt_trigger=None, radiant_trigger=None, force_trigger=None, multichannel=True, channels=None):
-        if channels == None:
-            channels = range(24)
+    def plot_event_by_id(self=None, i=None, station_number=None, run_number=None, event_number=None, lt_trigger=None, radiant_trigger=None, multichannel=True, channels=None):
         if i != None:
             station_number = self.header_df.station_number.iloc[i]
             run_number = self.header_df.run_number.iloc[i]
             event_number = self.header_df.event_number.iloc[i]
             lt_trigger = self.header_df.lt_triggers.iloc[i]
             radiant_trigger = self.header_df.radiant_triggers.iloc[i]
-            force_trigger = self.header_df.force_triggers.iloc[i]
 
         if lt_trigger == True:
             trigger_type = 'lt'
         elif radiant_trigger == True:
             trigger_type = 'radiant'
-        elif force_trigger == True:
-            trigger_type = 'force'
         else:
             trigger_type = 'Unknown'
 
@@ -278,7 +273,7 @@ class Flight:
             # setting labels
             ax0.plot([], [], label = 'trace')
             ax1.plot([], [], label = 'fourier transform')
-            for i in channels:
+            for i in range(24):
                 channel = station.get_channel(i)
                 trace = channel.get_trace()
                 times = channel.get_times()
